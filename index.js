ae96c15190f733772aa7fd36133194ecce4f8ea4
David Schoenholtz <dmschoenholtz@gmail.com>
2019-11-18 17:14:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function fetchBooks() {
-
+  fetch('http://api.open-notify.org/astros.json')
+.then(function(response) {
+  return response.json();
+}).then(function(json) {
+  console.log(json);
+});
 }
 
 function renderBooks(json) {
